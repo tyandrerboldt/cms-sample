576f948647b4b0fa664ead93db1e01215c6540bc
Tyander Boldt <tyandrer.boldt@gmail.com>
2024-11-26 17:57:35 -0300

    Fix use client annotation

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-export const dynamic = "force-dynamic";
-
 "use client";
+
+export const dynamic = "force-dynamic";
 
 import { Header } from "@/components/header";
 import { HeroCarousel } from "@/components/hero-carousel";
