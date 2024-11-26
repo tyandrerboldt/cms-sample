f950c45a6ca1dcebe6465b945cb8ca0099a9e1af
Tyander Boldt <tyandrer.boldt@gmail.com>
2024-11-26 17:20:06 -0300

    Other unused params

--- app/api/front/featured-packages/route.ts
+++ app/api/front/featured-packages/route.ts
@@ -1,10 +1,8 @@
 import { prisma } from '@/lib/prisma';
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 
-export async function GET(request: NextRequest) {
+export async function GET() {
   try {
-    const { searchParams } = request.nextUrl;
-
     const packages = await prisma.travelPackage.findMany({
       where: {
         status: "ACTIVE",
