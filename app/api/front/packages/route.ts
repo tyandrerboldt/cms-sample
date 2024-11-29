17c7944561054bcc9cf2e2a020ed87fed1a4c592
Tyander Boldt <tyandrer.boldt@gmail.com>
2024-11-29 13:34:52 -0300

    Add package dynamic

--- app/api/front/packages/route.ts
+++ app/api/front/packages/route.ts
@@ -1,6 +1,8 @@
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
+export const dynamic = 'force-dynamic'
+
 export async function GET(request: Request) {
   try {
     const { searchParams } = new URL(request.url);
