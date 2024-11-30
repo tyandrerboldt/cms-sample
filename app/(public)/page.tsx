6f52c2863f0b0ec958afcb3faed8fe208eba1314
Tyander Boldt <tyandrer.boldt@gmail.com>
2024-11-30 11:05:27 -0300

    Reorder section

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -26,8 +26,8 @@ export default function Home() {
   return (
     <PageTransition>
       <HeroCarousel />
-      <AboutSection />
       <FeaturedPackages />
+      <AboutSection />
       <BoatPackages />
       <CTASection />
       <LodgingPackages />
