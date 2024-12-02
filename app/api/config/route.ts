351e08a51b19298ba75900deeea1e0efa6e30953
Tyander Boldt <tyandrer.boldt@gmail.com>
2024-12-02 14:21:31 -0300

    Remove dynamic config

--- app/api/config/route.ts
+++ app/api/config/route.ts
@@ -1,8 +1,6 @@
 import { prisma } from "@/lib/prisma"
 import { NextResponse } from "next/server"
 
-export const dynamic = 'force-dynamic'
-
 export async function GET() {
   try {
     const config = await prisma.siteSettings.findFirst()
